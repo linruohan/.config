9069c0a12c464eb2762672dc2ce87aff57149bea
linruohan <mjt1220@126.com>
2021-06-03 21:40:37 +0800

    add mydock 5.3.9.5

---
+++ Mao_code_workspace/python_scripts/001.py
@@ -0,0 +1,67 @@
+
+#召唤系(Conjuration)：特技总数16
+player.addperk 000f2ca7 
+player.addperk 000c44bb 
+player.addperk 000c44bc 
+player.addperk 000c44bd 
+player.addperk 000c44be 
+player.addperk 000153ce 
+player.addperk 000640b3 
+player.addperk 000d799e 
+player.addperk 000d799c 
+player.addperk 000581dd 
+player.addperk 000581de 
+player.addperk 00105f30 
+player.addperk 00105f31 
+player.addperk 000cb419 
+player.addperk 000cb41a 
+player.addperk 000d5f1c 
+#
+#毁灭系(Destruction)：特技总数17
+player.addperk 000f2ca8 
+player.addperk 000c44bf 
+player.addperk 000c44c0 
+player.addperk 000c44c1 
+player.addperk 000c44c2 
+player.addperk 00105f32 
+player.addperk 000581e7 
+player.addperk 0010fcf8 
+player.addperk 000f392e 
+player.addperk 000581ea 
+player.addperk 0010fcf9 
+player.addperk 000f3933 
+player.addperk 00058200 
+player.addperk 0010fcfa 
+player.addperk 000f3f0e 
+player.addperk 000153cf 
+player.addperk 000153d2 
+#
+#恢复系(Restoration)：特技总数13
+player.addperk 000f2caa 
+player.addperk 000c44c7 
+player.addperk 000c44c8 
+player.addperk 000c44c9 
+player.addperk 000c44ca 
+player.addperk 000581f4 
+player.addperk 000581f5 
+player.addperk 000a3f64 
+player.addperk 000581f8 
+player.addperk 000581e4 
+player.addperk 000581f9 
+player.addperk 000153d1 
+player.addperk 00068bcc 
+
+#迷幻系(Illusion)：特技总数13
+player.addperk 000f2ca9 
+player.addperk 000c44c3 
+player.addperk 000c44c4 
+player.addperk 000c44c5 
+player.addperk 000c44c6 
+player.addperk 000581e1 
+player.addperk 000581e2 
+player.addperk 000581fd 
+player.addperk 00059b77 
+player.addperk 00059b78 
+player.addperk 000c44b5 
+player.addperk 00059b76 
+player.addperk 000153d0 
