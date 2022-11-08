7d6c9d3e370258cfe62b41dc0860f06d7907a8c5
linruohan <mjt1220@126.com>
2022-11-08 22:21:16 +0800

    rm unused files

--- install.config.sh
+++ install.config.sh
@@ -1,6 +1,14 @@
-git clone git@github.com:theniceboy/.config.git theniceboy.config
-cd theniceboy.config && {
-  patch < ./theniceboy.config.patch
-  cp -arv `ls | grep -v *.patch` .
-}
+# 1. install .config
+git clone git@github.com:theniceboy/.config.git ~/
+cd ~/.config patch < ./theniceboy.config.patch
 
+# 2. install nvim config
+#  2.1 theniceboy: https://github.com/theniceboy/nvim
+git clone git@github.com:theniceboy/nvim.git ~/.config
+# 打入自定义补丁
+cd ~/.config/nvim && patch < ../theniceboy_nvim.init.vim.patch
+#  2.2 git@github.com:LunarVim/Neovim-from-scratch.git
+git clone git@github.com:LunarVim/Neovim-from-scratch.git ~/.config
+
+#  2.3 git@github.com:askfiy/nvim.git
+git clone git@github.com:askfiy/nvim.git
