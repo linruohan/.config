00b28af1800552f44243d9df0417239bbb782577
linruohan <mjt1220@126.com>
2022-11-27 23:11:41 +0800

    7z .config/sublime

--- common/config_install.sh
+++ common/config_install.sh
@@ -10,14 +10,15 @@ config_install() {
     rt_log "theniceboy config installing ..."
     echo "install config files from theniceboy/.config: yes or no："
     isInstall=$(rt_select "yes no")
-    if [ "${isInstall}" != "yes" ]; then
+    [ "${isInstall}" != "yes" ] && {
         rt_warn "config install skipped ..."
         return 1
-    fi
-    git clone git@github.com:theniceboy/.config.git "${HOME}/theniceboy.config" && {
+			}
+    git clone https://github.com/theniceboy/.config.git "${HOME}/theniceboy.config" && {
         [ ! -d "$HOME/.config" ] && mkdir -p ~/.config
         cp -arv "${HOME}/theniceboy.config/"* "${HOME}/.config"
         cd ~/.config && patch < "${BIN:-}"/patch.files/theniceboy.config.patch
+				cd ~/.config && 7z x ~/codehub/*.config/.config.7z
         rm -rf ~/theniceboy.config
     }
     rt_log "theniceboy config cp done !"
